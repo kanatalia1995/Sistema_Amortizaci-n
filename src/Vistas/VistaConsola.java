d3c0f72cca4a1134f7323f504274acad24ef74df
JROrtiz112 <jrobertoortiz112@gmail.com>
2016-05-27 21:32:01 -0600

    Paquete de Vistas
    Creación del paquete vistas y la clase vista consola

---
+++ src/Vistas/VistaConsola.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vistas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class VistaConsola{
+    
+    public void menuConsola(){
+        while (true){
+            Scanner user_input=new Scanner(System.in);
+            String nombre=user_input.next();
+            
+        }
+    }
+}
